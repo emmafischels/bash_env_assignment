bab17021bb91de8dc19752bf0921ddefc14d30c3
Emma Fischels <emmafischels@ascend-rw02.ten.osc.edu>
2026-06-25 10:23:32 -0400

    Initial commit: environment setup scripts

---
+++ .ipynb_checkpoints/hello-checkpoint.R
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello, World!"
+
+#!/usr/bin/env python3
+print("Hello, World!")
+
+#!/usr/bin/env Rscript
+cat("Hello, World!\n")
---
+++ managing_files.sh
@@ -0,0 +1,11 @@
+mkdir bash_tutorial
+cd bash_tutorial
+mkdir new_folder
+touch file.txt
+cp file.txt copy.txt    #Copy a file 
+cp -r folder1 folder2   # Copy a directory and its contents 
+mv oldname.txt newname.txt  # Rename
+mv file.txt /path/to/dir/   # Move to another location
+rm file.txt          # Delete file
+rm -r folder_name    # Delete folder and contents
+rm -f folder_name.   # Force remove folder and contents
